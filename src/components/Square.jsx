308fa874999780f90b79c39c1cd8720d86f3a372
SONU <leflxlight@gmail.com>
2022-05-23 13:34:45 +0530

    sass

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
-
 function Square({ value }) {
-  return <button type="button">{value}</button>;
+  return (
+    <button type="button" className="square">
+      {value}
+    </button>
+  );
 }
 
 export default Square;
