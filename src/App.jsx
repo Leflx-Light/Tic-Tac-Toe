fd32922dd0e57e5da729892657a99ac03186032c
SONU <leflxlight@gmail.com>
2022-05-23 12:12:09 +0530

    Tic Tac Toe Day 1

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1>Welcome to React Vita Micro App</h1>
+      <p> Hard  to get more minimal than this React App</p>
+      <small>Hello this is me.</small>
+    </>
+  );
+};
+
+export default App;
