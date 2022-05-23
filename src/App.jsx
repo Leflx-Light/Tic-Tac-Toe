2db947deae0031390ad3186d725f583eac63b2cb
SONU <leflxlight@gmail.com>
2022-05-23 13:04:52 +0530

    Created Board and square component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import React from "react";
-
+import React from 'react';
+import Board from './components/Board.jsx';
 const App = () => {
   return (
-    <>
-      <h1>Welcome to React Vita Micro App</h1>
-      <p> Hard  to get more minimal than this React App</p>
-      <small>Hello this is me.</small>
-    </>
+    <div>
+      <h1>TIC TAC TOE</h1>
+      <Board/>
+    </div>
   );
 };
 
